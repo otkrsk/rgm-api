b432dd69f62b1fc1a6e8c289320a3cb65ea2a2f1
Nicholas Nuing <nicholas.nuing@gmail.com>
2020-01-23 16:43:49 +0800

    Combined Marker and Autocomplete with GoogleMap with ability to re-center and provide lat lng with search or marker drag

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
 import React from 'react';
-import {
-  GoogleMap,
-  LoadScript,
-  Marker
-} from '@react-google-maps/api';
-
-import WithAutocomplete from './Maps';
+import Map from './Maps';
 import './App.css';
-
-const position = {
-  lat: 3.040020,
-  lng: 101.579231
-}
-
-const onLoad = marker => {
-  console.log('marker: ', marker)
-}
-
-const onDragEnd = marker => {
-  console.log('marker: ', marker)
-  console.log('lat: ', marker.latLng.lat())
-  console.log('lng: ', marker.latLng.lng())
-}
 
 function App() {
 
   return (
-    <LoadScript
-      id="script-loader"
-      googleMapsApiKey="AIzaSyAtrfpL0BU0LzmP9rE5gqWP9yqZRJyBILk"
-      libraries={['places']}
-    >
-      <GoogleMap
-        id='example-map'
-        mapContainerStyle={{
-          height: "400px",
-          width: "650px"
+    <div style={{ margin: '10px' }}>
+      <Map
+        center={{
+          lat: 3.040020,
+          lng: 101.579231
         }}
+        height='500px'
         zoom={15}
-        center={position}
-      >
-        <Marker
-          onLoad={onLoad}
-          position={position}
-          draggable={true}
-          onDragEnd={onDragEnd}
-        />
-        <WithAutocomplete />
-      </GoogleMap>
-    </LoadScript>
+      />
+    </div>
   );
 }
 
--- src/Maps.js
+++ src/Maps.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import {
-  Autocomplete
+  GoogleMap,
+  LoadScript,
+  Autocomplete,
+  Marker
 } from '@react-google-maps/api';
 
-class WithAutocomplete extends Component {
+const places=['places'];
+
+class Map extends Component {
+
   constructor (props) {
     super(props)
 
@@ -11,17 +17,55 @@ class WithAutocomplete extends Component {
 
     this.onLoad = this.onLoad.bind(this)
     this.onPlaceChanged = this.onPlaceChanged.bind(this)
+
+    this.state = {
+      mapPosition: {
+        lat: this.props.center.lat,
+        lng: this.props.center.lng
+      },
+      markerPosition: {
+        lat: this.props.center.lat,
+        lng: this.props.center.lng
+      }
+    }
   }
 
   onLoad (autocomplete) {
     console.log('autocomplete: ', autocomplete)
-
     this.autocomplete = autocomplete
+  }
+
+  onDragEnd = marker => {
+    console.log('marker: ', marker)
+    console.log('lat: ', marker.latLng.lat())
+    console.log('lng: ', marker.latLng.lng())
+  }
+
+  onClick (e) {
+    console.log('clicked', e);
+    console.log('lat: ', e.latLng.lat());
+    console.log('lng: ', e.latLng.lng());
   }
 
   onPlaceChanged () {
     if (this.autocomplete !== null) {
       console.log(this.autocomplete.getPlace())
+      console.log('lat: ', this.autocomplete.getPlace().geometry.location.lat())
+      console.log('lng: ', this.autocomplete.getPlace().geometry.location.lng())
+
+      const latValue=this.autocomplete.getPlace().geometry.location.lat(),
+        lngValue=this.autocomplete.getPlace().geometry.location.lng();
+
+      this.setState({
+        markerPosition: {
+          lat: latValue,
+          lng: lngValue
+        },
+        mapPosition: {
+          lat: latValue,
+          lng: lngValue
+        },
+      })
     } else {
       console.log('Autocomplete is not loaded yet!')
     }
@@ -29,34 +73,70 @@ class WithAutocomplete extends Component {
 
   render () {
     return (
-      <Autocomplete
-        onLoad={this.onLoad}
-        onPlaceChanged={this.onPlaceChanged}
+      <LoadScript
+        id="script-loader"
+        googleMapsApiKey="AIzaSyAtrfpL0BU0LzmP9rE5gqWP9yqZRJyBILk"
+        libraries={places}
       >
-        <input
-          type="text"
-          placeholder="Customized your placeholder"
-          style={{
-            boxSizing: `border-box`,
-            border: `1px solid transparent`,
-            width: `240px`,
-            height: `32px`,
-            padding: `0 12px`,
-            borderRadius: `3px`,
-            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-            fontSize: `14px`,
-            outline: `none`,
-            textOverflow: `ellipses`,
-            position: "absolute",
-            left: "50%",
-            marginLeft: "-120px"
+        <GoogleMap
+          id='example-map'
+          mapContainerStyle={{
+            height: "400px",
+            width: "650px"
           }}
-        />
-      </Autocomplete>
+          zoom={15}
+          center={{
+            lat: this.state.mapPosition.lat,
+            lng: this.state.mapPosition.lng
+          }}
+          options={{
+            streetViewControl: false,
+            fullscreenControl: false,
+            mapTypeControl: false
+          }}
+          onClick={this.onClick}
+        >
+          <Marker
+            onLoad={this.onLoad}
+            position={{
+              lat: this.state.mapPosition.lat,
+              lng: this.state.mapPosition.lng
+            }}
+            draggable={true}
+            onDragEnd={this.onDragEnd}
+          />
+          <Autocomplete
+            onLoad={this.onLoad}
+            onPlaceChanged={this.onPlaceChanged}
+            restrictions={{country: "my"}}
+          >
+            <input
+              type="text"
+              placeholder="Search..."
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `300px`,
+                height: `32px`,
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `14px`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+                position: "absolute",
+                left: "50%",
+                marginLeft: "-120px",
+                marginTop: "11px"
+              }}
+            />
+          </Autocomplete>
+        </GoogleMap>
+      </LoadScript>
     )
   }
 
 
 }
 
-export default WithAutocomplete;
+export default Map;
