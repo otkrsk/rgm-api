04277daf92ed0f066ad46860db5219562ed96407
Nicholas Nuing <nicholas.nuing@gmail.com>
2020-01-23 15:23:07 +0800

    Added Marker and Autocomplete to GoogleMap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  GoogleMap,
+  LoadScript,
+  Marker
+} from '@react-google-maps/api';
+
+import WithAutocomplete from './Maps';
 import './App.css';
 
+const position = {
+  lat: 3.040020,
+  lng: 101.579231
+}
+
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
+
+const onDragEnd = marker => {
+  console.log('marker: ', marker)
+  console.log('lat: ', marker.latLng.lat())
+  console.log('lng: ', marker.latLng.lng())
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoadScript
+      id="script-loader"
+      googleMapsApiKey="AIzaSyAtrfpL0BU0LzmP9rE5gqWP9yqZRJyBILk"
+      libraries={['places']}
+    >
+      <GoogleMap
+        id='example-map'
+        mapContainerStyle={{
+          height: "400px",
+          width: "650px"
+        }}
+        zoom={15}
+        center={position}
+      >
+        <Marker
+          onLoad={onLoad}
+          position={position}
+          draggable={true}
+          onDragEnd={onDragEnd}
+        />
+        <WithAutocomplete />
+      </GoogleMap>
+    </LoadScript>
   );
 }
 
